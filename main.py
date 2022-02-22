8e484ff6ea5d3f704230b87496f4ee28027f6f50
Uziskull <jtunes_9@hotmail.com>
2022-02-22 09:37:45 +0000

    Adicionada opção de correr server sem DB dedicada

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ models.db.session.commit()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7777)
+    app.run(debug=True, port=7777, use_reloader=False)
--- website/__init__.py
+++ website/__init__.py
@@ -16,8 +16,10 @@ def start_server():
             db_url_prefix = 'DATABASE_URL='
             app.config['SQLALCHEMY_DATABASE_URI'] = [arg[len(db_url_prefix):] for arg in sys.argv if arg.startswith(db_url_prefix)][0]
         except IndexError:
-            print("Defina 'DATABASE_URL' nas variáveis de ambiente (ou nos argumentos de execução) com o URI do PostgreSQL!")
-            sys.exit(1)
+            #print("Defina 'DATABASE_URL' nas variáveis de ambiente (ou nos argumentos de execução) com o URI do PostgreSQL!")
+            #sys.exit(1)
+            print("A correr com base de dados em memória")
+            app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
     
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'mama que eh de uva'
