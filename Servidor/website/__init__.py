ca77ff4a623d6c0cfb4477d51e9d5c51d80867e5
Uziskull <jtunes_9@hotmail.com>
2023-01-25 00:49:03 +0000

    replaced db url to start with  instead

--- Servidor/website/__init__.py
+++ Servidor/website/__init__.py
@@ -11,8 +11,8 @@ def start_server():
     app.jinja_env.globals["ESTADOS_PROPOSTA"] = ESTADOS
 
     try:
-        #db_url = "postgresql:" + ":".join(os.environ['DATABASE_URL'].split(":")[1:])
-        db_url = os.environ['DATABASE_URL'] + "&sslrootcert=cockroachlabs-cluster.crt"
+        db_url = "cockroachdb:" + ":".join(os.environ['DATABASE_URL'].split(":")[1:]) + "&sslrootcert=cockroachlabs-cluster.crt"
+        #db_url = os.environ['DATABASE_URL'] + "&sslrootcert=Servidor/cockroachlabs-cluster.crt"
         app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     except KeyError:
         try:
